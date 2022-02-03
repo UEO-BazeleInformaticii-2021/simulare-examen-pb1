f308cc5ca9da63f62f855e847ed86b2b5c00dd43
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-02-03 22:44:31 +0200

    Factorial N cu recursivitate

---
+++ SimulareExamenProblema1/SimulareExamenProblema1/Recursivity.cs
@@ -0,0 +1,15 @@
+﻿namespace SimulareExamenProblema1
+{
+    public static class Recursivity
+    {
+        public static int Fact(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * Fact(n - 1);
+        }
+    }
+}
